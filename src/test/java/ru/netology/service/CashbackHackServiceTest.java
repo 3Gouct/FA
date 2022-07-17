dc3bb8fcb774478524ea1a57c005a9ac1ea3d6c7
3Gouct <ilya_football@mail.ru>
2022-07-17 21:22:32 +0300

    cheangesTests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,9 +7,8 @@ public class CashbackHackServiceTest {
     @org.testng.annotations.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService ();
-        int amount = 900;
         int expected = 100;
-        int result = service.remain (amount);
+        int result = service.remain (900);
         System.out.println (result);
 
         assertEquals (result, expected);
@@ -18,9 +17,8 @@ public class CashbackHackServiceTest {
     @org.testng.annotations.Test
     public void secondTestRemain() {
         CashbackHackService service = new CashbackHackService ();
-        int amount = 1100;
         int expected = 900;
-        int result = service.remain (amount);
+        int result = service.remain (1100);
         System.out.println (result);
 
         assertEquals (result, expected);
